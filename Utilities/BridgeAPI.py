ead5e9f5c08d2a09da8c188177918ab40619c6b8
Lars <l.cornelissen5@student.han.nl>
2022-03-30 16:49:01 +0200

    BridgeAPI refactor

--- Utilities/BridgeAPI.py
+++ Utilities/BridgeAPI.py
@@ -7,26 +7,24 @@ from scorch_api.bot import *
 API_KEY = os.getenv('TELOFIK_API_AUTH')
 BASE_PATH = getBasePath(__file__)
 
+NO_PERSONALIZED_MESSAGE = "LMAO %s, apparently you are not popular enough for a personalized message xd"
 MESSAGE_JSON = BASE_PATH + "/messages.json"
 
 def getMessageData():
     with open(MESSAGE_JSON, 'r') as file: return json.load(file)
 
 async def sendMessageAsTelofik(message):
-    channel = bot.get_channel(SWEATS_BRIDGE_CHANNEL)
-    await channel.send(embed=BMC.newMessage(title=message[:250]))
-    sendAPIRequest('/sendmessage', {"message": message})
+    await bot.get_channel(SWEATS_BRIDGE_CHANNEL).send(embed=BMC.newMessage(title=message[:250]))
+    sendAPIRequest('sendmessage', {"message": message})
 
 def getPlayerMeMessage(username):
-    data = getMessageData()
-    if username in data['personalized_texts']: return data['personalized_texts'][username]
-    else: return f"LMAO {username}, apparently you are not popular enough for a personalized message xd"
+    try: return getMessageData()['personalized_texts'][username]
+    except: return NO_PERSONALIZED_MESSAGE % username
 
 def getHealthMessage():
-    data = getMessageData()
-    return random.choice(data['texts'])
+    return random.choice(getMessageData()['texts'])
 
 def sendAPIRequest(endpoint, data): requests.post(f'{API_BASE}/{endpoint}', data=data, headers={"authorization": API_KEY})
-def whisperAsTelofik(message, username): sendAPIRequest('/whisper', {"username": username, "message": message})
-def kickPlayerFromGuild(username):  sendAPIRequest('/kick', {"username": username})
-def invitePlayerToGuild(username):  sendAPIRequest('/invite', {"username": username})
+def whisperAsTelofik(message, username): sendAPIRequest('whisper', {"username": username, "message": message})
+def kickPlayerFromGuild(username):  sendAPIRequest('kick', {"username": username})
+def invitePlayerToGuild(username):  sendAPIRequest('invite', {"username": username})
